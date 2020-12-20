9af7090ee19d5cb070fc31e58a1e03b7caf5f0e8
Fazalullah <fazalullahmemon52@gmail.com>
2020-12-20 15:00:57 +0500

    Added dynamic size calculation to the chart elements

--- lib/widgets/chartBar.dart
+++ lib/widgets/chartBar.dart
@@ -8,41 +8,44 @@ class ChartBar extends StatelessWidget {
   ChartBar(this.dayLabel, this.spendingAmount, this.spendingPctOfTotal);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            height: 20,
-            child: FittedBox(
-                child: Text('\Rs ${spendingAmount.toStringAsFixed(0)}'))),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(220, 220, 220, 1),
-                      border: Border.all(color: Colors.grey, width: 1.0),
-                      borderRadius: BorderRadius.circular(20))),
-              FractionallySizedBox(
-                heightFactor: spendingPctOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              )
-            ],
+    return LayoutBuilder(builder: (context, constrainsts) {
+      return Column(
+        children: [
+          Container(
+              height: constrainsts.maxHeight * 0.15,
+              child: FittedBox(
+                  child: Text('\Rs ${spendingAmount.toStringAsFixed(0)}'))),
+          SizedBox(
+            height: constrainsts.maxHeight * 0.05,
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(dayLabel)
-      ],
-    );
+          Container(
+            height: constrainsts.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(220, 220, 220, 1),
+                        border: Border.all(color: Colors.grey, width: 1.0),
+                        borderRadius: BorderRadius.circular(20))),
+                FractionallySizedBox(
+                  heightFactor: spendingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: constrainsts.maxHeight * 0.05,
+          ),
+          Container(
+              height: constrainsts.maxHeight * 0.15, child: Text(dayLabel))
+        ],
+      );
+    });
   }
 }
